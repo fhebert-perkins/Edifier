d55873bbb954c9eeb7aa59a8dc1b46fc984c9477
Finley Hebert-Perkins <fhebert-perkins16@bayschoolsf.org>
2016-07-16 07:54:10 -0400

    Fixing some stuff

--- Edifier/chrome/content.js
+++ Edifier/chrome/content.js
@@ -12,18 +12,18 @@ function change(element){
 
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
-                var replacedText = text.replace(/\bGod\b/gi, 'Ed');
-                var replacedText = replacedText.replace(/\bgod\b/gi, 'ed');
+                var replacedText = text.replace(/God/gi, 'Ed');
+                var replacedText = replacedText.replace(/god/gi, 'ed');
                 var replacedText = replacedText.replace(/\bThor\b|\bthor\b/gi, "Norse Thunder Ed");
                 var replacedText = replacedText.replace(/\bOdin\b|\bodin\b/gi, "Norse One-Eyed Ed");
                 var replacedText = replacedText.replace(/\bLoki\b|\bloki\b/gi, "Norse Lying Ed");
                 var replacedText = replacedText.replace(/\bZeus\b|\bzeus\b/gi, "Greek Thunder Ed");
-                var replacedText = replacedText.replace(/\bPosiedon\b|\bposiedon\b/gi, "Greek Ed of the Ocean Floor");
+                var replacedText = replacedText.replace(/\bPoseidon\b|\bposiedon\b/gi, "Greek Ed of the Ocean Floor");
                 var replacedText = replacedText.replace(/\bHephestos\b|\bhephestos\b/gi, "Greek Ed of the Forge");
                 var replacedText = replacedText.replace(/\bHades\b|\bhades\b/gi, "Greek Ed of Death");
-                var replacedText = replacedText.replace(/\bAthena\b|\bathena\b/gi, "Greek Ed-ess of Wisdom");
-                var replacedText = replacedText.replace(/\bArtemis\b|\bartemis\b/gi, "Greek Maiden Ed-ess of the Hunt");
-                var replacedText = replacedText.replace(/\bAphrodite\b|\baphrodite\b/gi, "Greek All Sexy Like Ed-ess")
+                var replacedText = replacedText.replace(/\bAthena\b|\bathena\b/gi, "Greek Edess of Wisdom");
+                var replacedText = replacedText.replace(/\bArtemis\b|\bartemis\b/gi, "Greek Maiden Edess of the Hunt");
+                var replacedText = replacedText.replace(/\bAphrodite\b|\baphrodite\b/gi, "Greek All Sexy Like Edess")
 
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
