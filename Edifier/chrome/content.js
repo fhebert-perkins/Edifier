37f4e671abc1eb25c22ebc2c6e37a62b76961d70
Finley Hebert-Perkins <fhebert-perkins16@bayschoolsf.org>
2016-07-16 08:09:18 -0400

    all greek eds covered

--- Edifier/chrome/content.js
+++ Edifier/chrome/content.js
@@ -19,11 +19,20 @@ function change(element){
                 var replacedText = replacedText.replace(/\bLoki\b|\bloki\b/gi, "Norse Lying Ed");
                 var replacedText = replacedText.replace(/\bZeus\b|\bzeus\b/gi, "Greek Thunder Ed");
                 var replacedText = replacedText.replace(/\bPoseidon\b|\bposiedon\b/gi, "Greek Ed of the Ocean Floor");
-                var replacedText = replacedText.replace(/\bHephestos\b|\bhephestos\b/gi, "Greek Ed of the Forge");
+                var replacedText = replacedText.replace(/\bHephaestus\b|\bhephaestus\b/gi, "Greek Ed of the Forge");
                 var replacedText = replacedText.replace(/\bHades\b|\bhades\b/gi, "Greek Ed of Death");
                 var replacedText = replacedText.replace(/\bAthena\b|\bathena\b/gi, "Greek Edess of Wisdom");
                 var replacedText = replacedText.replace(/\bArtemis\b|\bartemis\b/gi, "Greek Maiden Edess of the Hunt");
-                var replacedText = replacedText.replace(/\bAphrodite\b|\baphrodite\b/gi, "Greek All Sexy Like Edess")
+                var replacedText = replacedText.replace(/\bAphrodite\b|\baphrodite\b/gi, "Greek All Sexy Like Edess");
+                var replacedText = replacedText.replace(/\bApollo\b|\bapollo\b/gi, "Greek Ed of Medicine");
+                var replacedText = replacedText.replace(/\bAres\b|\bares\b/gi, "Greek Ed of War");
+                var replacedText = replacedText.replace(/\bDemeter\b|\bdemeter\b/gi, "Greek Ed of Grain");
+                var replacedText = replacedText.replace(/\bDionysus\b|\bdionysus\b/gi, "Greek Ed of Partying");
+                var replacedText = replacedText.replace(/\bHera\b|\bhera\b/gi, "Greek Edess of Women");
+                var replacedText = replacedText.replace(/\bHermes\b|\bhermes\b/gi, "Greek Ed of Travel");
+                var replacedText = replacedText.replace(/\bHestia\b|\bhestia\b/gi, "Greek Edess of the Hearth");
+                var replacedText = replacedText.replace(/Giant|giant/gi, "big man");
+
 
                 if (replacedText !== text) {
                     element.replaceChild(document.createTextNode(replacedText), node);
